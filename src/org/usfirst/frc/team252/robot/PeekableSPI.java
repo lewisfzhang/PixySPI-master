77fbf77eb849fb780e397f196a99758a87b37791
0xFireball <0xFireball@outlook.com>
2017-01-13 18:53:41 -0800

    add the rest of the block sync stuff

--- src/org/usfirst/frc/team252/robot/PeekableSPI.java
+++ src/org/usfirst/frc/team252/robot/PeekableSPI.java
@@ -15,8 +15,9 @@ public class PeekableSPI {
 	}
 	
 	public void writeByte(int b) {
-		byte[] buf = new byte[] {(byte)b};
-		spi.transaction(buf, new byte[buf.length], buf.length);
+		//byte[] buf = new byte[] {(byte)b};
+		//spi.transaction(buf, new byte[buf.length], buf.length);
+		spi.write(new byte[] { (byte)b }, 1);		
 	}
 	
 	public int readWord() {
@@ -30,16 +31,40 @@ public class PeekableSPI {
 	}
 	
 	private void getNextWord() {
-		byte[] buf = new byte[2];
-		spi.transaction(new byte[buf.length], buf, buf.length);
-		nextWord = makeWord(buf[0], buf[1]);
+		//spi.transaction(new byte[buf.length], buf, buf.length);
+		byte[] buf = readSyncedBytes(2);
+		// this is big endian (MSB, LSB)
+		nextWord = makeWord(buf[1], buf[0]);
 	}
 	
 	private static int makeWord(byte lsb, byte msb) {
 		return Byte.toUnsignedInt(lsb) | Byte.toUnsignedInt(msb) << 8;
 	}
 	
+	private byte[] readSyncedBytes(int len)
+	{
+		byte[] r = new byte[len];
+		for (int i=0;i<len;i++)
+		{
+			r[i] = readSyncedByte();
+		}
+		return r;
+	}
+	
+	private byte readSyncedByte()
+	{
+		// send sync byte
+		spi.write(new byte[] {PIXY_SYNC_BYTE }, 1);
+		// read data
+		byte[] buf = new byte[1];
+		spi.read(false, buf, buf.length);
+		return buf[0];
+	}
+	
 	private SPI spi;
 	private int nextWord;
+	
+	private final int PIXY_SYNC_BYTE = 0x5a;
+	private final int PIXY_SYNC_BYTE_DATA = 0x5b;
 	
 }
