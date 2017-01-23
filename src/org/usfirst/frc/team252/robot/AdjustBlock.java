17423579d343ed42622e2e038e72e578734cd78f
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2017-01-23 09:59:44 -0800

    Right method of doing it. Will start on actualy undistortment tonight

--- src/org/usfirst/frc/team252/robot/AdjustBlock.java
+++ src/org/usfirst/frc/team252/robot/AdjustBlock.java
@@ -1,33 +1,21 @@
 package org.usfirst.frc.team252.robot;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-
-import javax.imageio.ImageIO;
-
 import org.usfirst.frc.team252.robot.Frame.Block;
 
 public class AdjustBlock {
 	public Block AdjustBlock(Block block) {
-		BufferedImage img = new BufferedImage(block.width, block.height, 1); // 1 is RGB
-		img = rawToImage(block);
-		img = undistortImage(img);
-		block = imageToRaw(img);
+		block.centerX = undistortX(block.centerX);
+		block.centerY = undistortY(block.centerY);
 		return block;
 	}
 
-	private BufferedImage rawToImage(Block block) {
-		// TODO Auto-generated method stub
-		return null;
+	private int undistortY(int y) {
+		int yCorrect = 0;
+		return yCorrect;
 	}
-	
-	private BufferedImage undistortImage(BufferedImage img) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-		
-	private Block imageToRaw(BufferedImage img) {
-		// TODO Auto-generated method stub
-		return null;
+
+	private int undistortX(int x) {
+		int xCorrect = 0;
+		return xCorrect;
 	}
 }
