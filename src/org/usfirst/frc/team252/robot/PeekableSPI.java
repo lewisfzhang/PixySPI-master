2f35c889f954d19dc2dc7691260cb68ac2061d52
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2017-02-07 11:01:00 -0800

    Simpler SPI. Not sure if works

--- src/org/usfirst/frc/team252/robot/PeekableSPI.java
+++ src/org/usfirst/frc/team252/robot/PeekableSPI.java
@@ -1,93 +1,24 @@
 package org.usfirst.frc.team252.robot;
 
-import java.util.List;
-
 import edu.wpi.first.wpilibj.SPI;
 
 public class PeekableSPI {
-	
+	byte[] data;
 	public PeekableSPI(SPI spi) {
 		this.spi = spi;
-		getNextWord();
+		data = getByte(this.spi);
 	}
 	
-	private byte getByte(byte data)
-	{
-		byte[] send = new byte[] {data};
-		byte[] recv = new byte[send.length];
-		spi.transaction(send, recv, recv.length);
-		return recv[0];
+	private byte[] getByte(SPI spi) {
+		byte[] dataReceived = new byte[14];
+		spi.read(false, dataReceived, dataReceived.length);
+		return dataReceived;
 	}
 	
-	private int getWord()
-	{
-		return makeWord(getByte((byte)0), getByte((byte)0));
+	public int readWord(byte a, byte b) {
+		int val = ((a & 0xff) << 8) | (b & 0xff);
+		return val;
 	}
-	
-	public int readByte() {
-		int b = peekByte();
-		getNextByte();
-		return b;
-	}
-	
-	public int peekByte() {
-		return nextWord >>> 8;
-	}
-	
-	private void getNextByte() {
-		int b = Byte.toUnsignedInt(getByte((byte)0));
-		nextWord = ((nextWord&0xFF) << 8) | b;
-	}
-	
-	public int readWord() {
-		int word = peekWord();
-		getNextWord();
-		return word;
-	}
-	
-	public int peekWord() {
-		return nextWord;
-	}
-	
-	private void getNextWord() {
-		nextWord = getWord();
-		++wordsRead;
-	}
-	
-	private static int makeWord(byte msb, byte lsb) {
-		return Byte.toUnsignedInt(lsb) | Byte.toUnsignedInt(msb) << 8;
-	}
-	
-	public long getWordsRead() { return wordsRead; }
 	
 	private SPI spi;
-	private int nextWord;
-	private long wordsRead = 0;
-	
-	
-	
-	
-	public static String hexByte(int b) {
-		String hex = Integer.toHexString(b);
-		if (hex.length() < 2) hex = "0"+hex;
-		return hex;
-	}
-	public static void visualizeBytes(List<Integer> byteStream) {
-		System.out.println(byteStream.size()+" bytes:");
-		int numZeros = 0;
-		for (int i : byteStream) {
-			if (i == 0) {
-				numZeros++;
-			} else {
-				if (numZeros > 5) System.out.println("\n---- "+numZeros+" ZEROS ----");
-				else for (; numZeros > 0; numZeros--) System.out.print("00 ");
-				numZeros = 0;
-				System.out.print(hexByte(i)+" ");
-			}
-		}
-		if (numZeros > 5) System.out.println("\n---- "+numZeros+" ZEROS ----");
-		else for (; numZeros > 0; numZeros--) System.out.print("00 ");
-		System.out.println();
-	}
-	
 }
--- src/org/usfirst/frc/team252/robot/PixyCam.java
+++ src/org/usfirst/frc/team252/robot/PixyCam.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc.team252.robot;
 
+import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.usfirst.frc.team252.robot.Frame.Block;
 import edu.wpi.first.wpilibj.SPI;
 
 public class PixyCam {
-	
+    public static double fx, fy, cx, cy, k1, k2, k3;
 	public PixyCam() {
 		SPI spi = new SPI(SPI.Port.kOnboardCS0);
 		spi.setMSBFirst();
@@ -18,39 +20,36 @@ public class PixyCam {
 	}
 	
 	public Frame.Block parseBlock() {
+		//get Camera Matrix and Distortion Coefficients
+		 
 		Frame.Block block = new Frame.Block();
 		
 		// Wait for sync
-		int lastByte = 0x00;
-		while (true) {
-			int curByte = pspi.readByte();
-			if (lastByte==0xaa && curByte==0x55) break;
-			lastByte = curByte;
-		}
-		
+		while (pspi.readWord(pspi.data[0], pspi.data[1]) != 0xaa55) {}
 		// check if there's another sync word
-		if (pspi.peekWord() == 0xaa55) {
-			return null;
-		}
 		
 		// read the block data
-		block.checksum = pspi.readWord();
-		block.signature = pspi.readWord();
-		block.centerX = pspi.readWord();
-		block.centerY = pspi.readWord();
-		block.width = pspi.readWord();
-		block.height = pspi.readWord();
-		int chk = block.signature+block.centerX+block.centerY+block.width+block.height;
+		block.checksum = pspi.readWord(pspi.data[2], pspi.data[3]);
+		block.signature = pspi.readWord(pspi.data[4], pspi.data[5]);
+		block.centerX = pspi.readWord(pspi.data[6], pspi.data[7]);
+		block.centerY = pspi.readWord(pspi.data[8], pspi.data[9]);
+		block.width = pspi.readWord(pspi.data[10], pspi.data[11]);
+		block.height = pspi.readWord(pspi.data[12], pspi.data[13]);
+		Point center = transformCoordinates(block.centerX, block.centerY);
+		double chk = block.signature+block.centerX+block.centerY+block.width+block.height;
 		if (block.checksum != chk) {
-			System.out.println("BLOCK HAD AN INVALID CHECKSUM ("+Integer.toHexString(block.checksum)+", should be "+Integer.toHexString(chk)+")");
+			System.out.println("BLOCK HAD AN INVALID CHECKSUM ("+Integer.toHexString(block.checksum)+", should be "+Double.toHexString(chk)+")");
 			return null;
 		}
+        block.centerX = center.x;
+        block.centerY = center.y;
+        undistortFourCorners(block);
 		return block;
 	}
 	
 	public Frame getFrame() {
 		List<Frame.Block> blocks = new ArrayList<>();
-		while (true) { // don't judge, it werks
+		while (true) { // don't judge, it works
 			Frame.Block b = parseBlock();
 			if (b == null) break;
 			blocks.add(b);
@@ -65,7 +64,45 @@ public class PixyCam {
 		
 		return new Frame(blocks, frameCount++);
 	}
-	
+	private void undistortFourCorners(Block block) {
+        // based on the inverted plane of a camera, you can calculate the four side x and y values of the rectangular block
+        double leftSideX = block.centerX-(block.width/2);
+        double rightSideX = block.centerX+(block.width/2);
+        double topSideY = block.centerY-(block.height/2);
+        double bottomSideY = block.centerY+(block.height/2);
+
+        // Undistort the two opposite points of the rectangular block
+        Point bottomLeft = transformCoordinates(leftSideX, bottomSideY);
+        Point topRight = transformCoordinates(rightSideX, topSideY);
+
+        // Calculate the width and height based on opposite points of rectangular block
+        block.width = topRight.x - bottomLeft.x;
+        block.height = bottomLeft.y - topRight.y;
+    }
+    private static Point transformCoordinates(double xDistorted, double yDistorted) {
+        // put in terms of cx and cy being the origin and normalize
+        double xDistNormalized = (xDistorted - cx)/400;
+        double yDistNormalized = (yDistorted - cy)/400;
+        // apply undistortion
+        double rDistorted = Math.hypot(xDistNormalized, yDistNormalized);
+        double rUndistorted = radiusFuncInv(rDistorted);
+        double xUndistorted = xDistNormalized*rUndistorted/rDistorted;
+        double yUndistorted = yDistNormalized*rUndistorted/rDistorted;
+        // convert back to pixel space and return
+        Point undistort = new Point((int) (Math.round(xUndistorted*400 + cx)), (int) (Math.round(yUndistorted*400 + cy)));
+        return undistort;
+    }
+    private static double radiusFuncInv(double rDistorted) { // Function for Undistortion
+        double rSquared = rDistorted*rDistorted;
+        double rUndistorted = rDistorted;
+        rDistorted *= rSquared;
+        rUndistorted -= k1*rDistorted;
+        rDistorted *= rSquared;
+        rUndistorted += (3*k1*k1 - k2)*rDistorted;
+        rDistorted *= rSquared;
+        rUndistorted += (-12*k1*k1*k1 + 8*k1*k2 - k3)*rDistorted;
+        return rUndistorted;
+    }
 //	public void setServoPosition(int pan, int tilt) {
 //		pspi.writeWord(0xff00);
 //		pspi.writeWord(pan);
