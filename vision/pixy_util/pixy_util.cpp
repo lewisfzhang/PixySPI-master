bfb963e57bcb4f8ca12408e74407dedeceed12a8
SumiGovindaraju <sumi.govindaraju20@bcp.org>
2017-10-07 17:50:54 -0700

    Fine tuned thresholding w/ iPhone flashlight and added logic to prevent errors

--- vision/pixy_util/pixy_util.cpp
+++ vision/pixy_util/pixy_util.cpp
@@ -33,22 +33,20 @@ inline void interpolateBayer(uint16_t width, uint16_t x, uint16_t y, uint8_t *pi
     }
 }
 
-cv::Mat renderBA81(uint8_t renderFlags, uint16_t width, uint16_t height, uint32_t frameLen, uint8_t *frame) {
-    std::cout << "Started renderBA81" << std::endl;
-    
+cv::Mat renderBA81(uint8_t renderFlags, uint16_t width, uint16_t height, uint32_t frameLen, uint8_t *frame) {    
     uint16_t x, y;
     uint8_t r, g, b;
     cv::Mat imageRGB;
 
     frame += width;
-    std::cout << "Starting to create array [" << 3*((height-2)*(width-2)) << "]" << std::endl;        
-    uchar *data = new uchar[3*((height-2)*(width-2))];
-    std::cout << "Created array" << std::endl;        
+
+    uchar *data = new uchar[3 * ((height - 2) * (width - 2))];
+
     uint m = 0;
 
-    for (y=1; y<height-1; y++) {
+    for (y = 1; y < height - 1; y++) {
         frame++;
-        for (x=1; x<width-1; x++, frame++) {
+        for (x = 1; x < width - 1; x++, frame++) {
             interpolateBayer(width, x, y, frame, &r, &g, &b);
             data[m++] = b;
             data[m++] = g;
@@ -59,40 +57,29 @@ cv::Mat renderBA81(uint8_t renderFlags, uint16_t width, uint16_t height, uint32_
 
     imageRGB = cv::Mat(height - 2, width - 2, CV_8UC3, data);
     delete[] data;
-    std::cout << "Ended renderBA81" << std::endl;    
 
     return imageRGB;
 }
 
 static cv::Mat processImage(cv::Mat src) {
     cv::Mat dst;
-    std::cout << "Initialized dst" << std::endl;
     src.copyTo(dst);
-    std::cout << "Copied src to dst" << std::endl;    
 
-    cv::cvtColor(dst, dst, CV_BGR2HSV);
-    std::cout << "Converted dst from BGR to HSV" << std::endl;    
-
-    inRange(dst, cv::Scalar(0, 0, 0), cv::Scalar(90, 128, 128), dst);
-    std::cout << "Thresholded dst" << std::endl;    
+    inRange(dst, cv::Scalar(120, 190, 225), cv::Scalar(255, 255, 255), dst);    
 
     std::vector<std::vector<cv::Point> > contours;
-    std::cout << "Initialized countours" << std::endl;    
     std::vector<cv::Vec4i> hierarchy;
-    std::cout << "Iniatlized hierarchy" << std::endl;
     
     cv::findContours(dst, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE);
-    std::cout << "Found countours" << std::endl;    
 
-    for(int i = 0; i >= 0; i = hierarchy[i][0]) {
-        std::cout << "Started for iteration" << std::endl;        
+    std::cout << "Countours Size: " << contours.size() << std::endl;    
+    std::cout << "Hierarchy Size: " << hierarchy.size() << std::endl;
+    for(int i = 0; contours.size() != 0 && hierarchy.size() != 0 && i >= 0; i = hierarchy[i][0]) {
         cv::Scalar color(180, 255, 255);
-        std::cout << "Initialized color" << std::endl;        
         drawContours(dst, contours, i, color, CV_FILLED, 8, hierarchy);
-        std::cout << "Drew countours" << std::endl;        
     }
 
-    std::cout << "Ended for loop" << std::endl;    
+    // cv::morphologyEx(dst, dst, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(1, 1)));
 
     return dst;
 }
@@ -116,7 +103,11 @@ cv::Mat getImage() {
                                  0,              // separator
                                  &response, &fourcc, &renderflags, &rwidth, &rheight, &numPixels, &pixels, 0);
 
-    return processImage(renderBA81(renderflags,rwidth,rheight,numPixels,pixels));
+    std::cout << "Return Value of pixy_command: " << return_value << std::endl;
+
+    if (return_value >= 0 && rwidth > 2 && rheight > 2)
+        return processImage(renderBA81(renderflags,rwidth,rheight,numPixels,pixels));
+    return cv::Mat::zeros(320, 200, CV_8UC3);        
 }
 
 cv::Mat pixy_util::nextImage() {
@@ -140,9 +131,7 @@ bool pixy_util::init() {
 }
 
 int main() {
-    if (pixy_util::init()) {
-        std::cout << "Connected to Pixy over USB" << std::endl;
-        
+    if (pixy_util::init()) {       
         while (cvWaitKey(1) == -1) {
             imshow("Pixy Feed", pixy_util::nextImage());
         }
